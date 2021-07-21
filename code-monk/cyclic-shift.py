a4dcb18744126f01a33cf10870d10db8c397d8e3
Arnab Mukherjee <am0072008@gmail.com>
2021-07-21 23:31:29 +0530

    cyclic shift take 2

--- code-monk/cyclic-shift.py
+++ code-monk/cyclic-shift.py
@@ -1,17 +1,21 @@
+def shift(value: str):
+    return value[1:] + value[0]
+
+
+def get_max(value: str) -> str:
+    max_ = float("-inf")
+    count_ = 0
+    while count_ < len(value):
+        if max_ < int(value, 2):
+            max_ = int(value, 2)
+        value = shift(value)  # shift
+        count_ += 1
+    return bin(max_)[2:]
 
 
 def cyclic_shift(value: str, k_: str):
-    max_ = float("-inf")
-    count_ = 0
-    rotations = 0
-    while count_ < len(value):
-        print(count_, value, len(value), max_)
-        if max_ < int(value, 2):
-            max_ = int(value, 2)
-            rotations += 1
-        value = value[1:] + value[0]  # shift
-        count_ += 1
-    return bin(max_), rotations
+    max_bin = get_max(value)
+    return max_bin
 
 
 if __name__ == '__main__':
