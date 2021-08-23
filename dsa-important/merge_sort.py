75afc11dc6e1c43cd75aa594eaae8030400f802a
Arnab Mukherjee <am0072008@gmail.com>
2021-08-23 22:33:46 +0530

    merge sort

---
+++ dsa-important/merge_sort.py
@@ -0,0 +1,46 @@
+from typing import List
+
+
+def merge(l1: List, l2: List) -> List:
+    # merge routine
+    l = r = 0
+    l3 = []
+
+    while l < len(l1) and r < len(l2):
+        if l1[l] <= l2[r]:
+            l3.append(l1[l])
+            l += 1
+
+        elif l1[l] > l2[r]:
+            l3.append(l2[r])
+            r += 1
+
+    # remaining elements
+    while l < len(l1):
+        l3.append(l1[l])
+        l += 1
+
+    while r < len(l2):
+        l3.append(l2[r])
+        r += 1
+
+    return l3
+
+
+def merge_recursive(l: List) -> List:
+    if len(l) < 2:
+        # recursive termination condition
+        return l
+
+    middle = len(l)//2
+    s1 = merge_recursive(l[:middle])
+    s2 = merge_recursive(l[middle:])
+    return merge(s1, s2)
+
+
+if __name__ == '__main__':
+    a = [1, 3, 90, 113]
+    b = [2, 8, 56]
+    print(merge(a, b))
+    c = [8, 2, 56, 0, 2, 4, 6, 10, 3, 1]
+    print(merge_recursive(c))
