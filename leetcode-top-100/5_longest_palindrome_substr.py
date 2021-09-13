006487561153f730af399262f3aaf9aae0a0335d
Arnab Mukherjee <am0072008@gmail.com>
2021-09-13 15:40:50 +0530

    longest palindrome updates

--- leetcode-top-100/5_longest_palindrome_substr.py
+++ leetcode-top-100/5_longest_palindrome_substr.py
@@ -1,4 +1,7 @@
 # DP approach, show the brute force approach every were.
+# all substr in O(n^2) complexity and checking palindrome O(n) for each substr.
+# Total O(n^3) complexity
+
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
@@ -21,7 +24,7 @@ class Solution:
 
         # generating remaining elements
         length = 2
-        max_length = 2
+        max_length = -1
         start_index = 0
         while length <= len(s):
             start = 0
@@ -32,11 +35,49 @@ class Solution:
                     if length >= max_length:
                         max_length = length
                         start_index = start
+                print(start, length, max_length, s[start], s[end], db[start+1][end-1])
                 start += 1
             length += 1
         return s[start_index:start_index+max_length]
 
 
+class Solution_jag:
+    def longestPalindrome(self, s: str) -> str:
+        length = len(s)
+        count = 2
+        max_length = 0
+        start = 0
+        end = 0
+        dp = [
+            [0 for i in range(len(s))] for j in range(len(s))
+        ]
+
+        for i in range(length-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = 1
+                if count > max_length:
+                    start = i
+                    end = i+1
+                    max_length = count
+            else:
+                dp[i][i + 1] = 0
+            dp[i][i] = 0
+
+        for gap in range(2, length):
+            for i in range(length-gap):
+                j = gap + i
+
+                if (s[i] == s[j]) and (dp[i+1][j-1] == 1):
+                    dp[i][j]=1
+                    count = j-i+1
+                    if count > max_length:
+                        start = i
+                        end = i + 1
+                        max_length = count
+
+        return s[start:end+1]
+
+
 if __name__ == '__main__':
-    print(Solution().longestPalindrome("cbbp"))  # this use case is failing
+    # print(Solution().longestPalindrome("cbbp"))  # this use case is failing
     print(Solution().longestPalindrome("aaaabbaa"))
