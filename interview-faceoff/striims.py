a6be7da36251b6d7b9471232826bd94d7330cf37
Arnab Mukherjee <am0072008@gmail.com>
2021-09-20 13:12:08 +0530

    striims updates

--- interview-faceoff/striims.py
+++ interview-faceoff/striims.py
@@ -3,5 +3,9 @@
 # Find max power distribution.
 
 # power of 2 and sum of digits till we get single digit
+# any number % 9 == 0 sum aff all digits till single = 9
+# any number can be represent as n = 9x + k so n % 9
 
 # can we make a list strictly increasing by doing  A[i] - K, where A[0] <= K < =A[N]
+# if A[i-1] <= A[i] <= A[i+1] then ok
+# else A[i] = A[i-1] + A[i+1] // 2 but A[i] != A[i+1] != A[i-1]
