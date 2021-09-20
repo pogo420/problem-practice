29a936c054d06cfd5f70a908946c7adc79e4fd3d
Arnab Mukherjee <am0072008@gmail.com>
2021-09-20 12:21:37 +0530

    striiims interview hacker questions

---
+++ interview-faceoff/striims.py
@@ -0,0 +1,7 @@
+# Max distribution between n elements.
+# i -> j dissipation(K) during transfer
+# Find max power distribution.
+
+# power of 2 and sum of digits till we get single digit
+
+# can we make a list strictly increasing by doing  A[i] - K, where A[0] <= K < =A[N]
