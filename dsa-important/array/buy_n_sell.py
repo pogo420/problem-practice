b76e4a0542429585a97f225a668707955824f066
Arnab Mukherjee <am0072008@gmail.com>
2021-09-23 22:54:35 +0530

    buy n sell stocks

--- dsa-important/array/buy_n_sell.py
+++ dsa-important/array/buy_n_sell.py
@@ -34,7 +34,21 @@ def max_profit_2(data: list) -> int:
     return max_diff
 
 
+def max_profit_multiple_sell(data: list):
+    profit = 0
+    i = 1
+    while i < len(data):
+        if data[i] > data[i-1]:  # buy during local min , sell during local max
+            profit += (data[i] - data[i-1])
+        i += 1
+    return profit
+
+
 if __name__ == '__main__':
     a = [3, 1, 4, 8, 7, 2, 5]
     print(max_profit_1(a))
     print(max_profit_2(a))
+
+    s = [5, 2, 7, 3, 6, 1, 2, 4]
+    print(max_profit_multiple_sell(s))
+
