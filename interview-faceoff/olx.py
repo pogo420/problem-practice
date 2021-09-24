8743eaf043ceba4c58c39519c640a545ad1ffe16
Arnab Mukherjee <am0072008@gmail.com>
2021-09-24 12:53:18 +0530

    olx interview

--- interview-faceoff/olx.py
+++ interview-faceoff/olx.py
@@ -1,1 +1,12 @@
 # valid ip addresses from a string
+# Streams sorting - heap.
+# CAP theorem.
+# As about the questions to design:
+# 	- user
+#   - volume/size of messages.
+# 	- databases.
+# 	- analytics.
+# Kubernetes - Cordon  - nodes.
+# Max: sum of two elements and propagating.
+# Notification service.
+# How to give system design interview.
