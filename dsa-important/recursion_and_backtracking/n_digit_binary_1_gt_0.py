b49adac1de72005a6744e98ec2390d8ead86d778
Arnab Mukherjee <am0072008@gmail.com>
2021-11-07 11:23:27 +0530

    1/0 binary bits

--- dsa-important/recursion_and_backtracking/n_digit_binary_1_gt_0.py
+++ dsa-important/recursion_and_backtracking/n_digit_binary_1_gt_0.py
@@ -14,9 +14,30 @@ def get_digits(n: int, zeros: int, ones: int, pos: int, result: list, current: l
         get_digits(n - 1, zeros + 1, ones, pos + 1, result, current)
 
 
+def binary_digits(N, curr, result, ones, zeros, pos):
+
+    if zeros + ones == N:
+        result.append(curr.copy())
+        return
+
+    if zeros + ones < N:
+        curr[pos] = 1
+        binary_digits(N, curr, result, ones + 1, zeros, pos + 1)
+
+    if ones > zeros:
+        curr[pos] = 0
+        binary_digits(N, curr, result, ones, zeros + 1, pos + 1)
+
+
 if __name__ == '__main__':
     n = 3
     r = []
     c = [0] * n
     get_digits(n, 0, 0, 0, r, c)
+    print(r)
+
+    n = 3
+    r = []
+    c = [0] * n
+    binary_digits(n, c, r, 0, 0, 0)
     print(r)
